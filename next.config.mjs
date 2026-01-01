2e6f8a2b9d1e0e1097e7d1da3dce390fbe55f6c5
inderjit-21 <inderjit.contact@gmail.com>
2026-01-01 18:30:08 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  turbopack: {},
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+    return config;
+  },
   reactCompiler: true,
 };
 
